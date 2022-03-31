fbe45bb92986a7170014e272ebed1190ebdc7480
docavd <aspadonet@gmail.com>
2022-03-31 11:40:25 +0300

    final

--- VerificationEmail/Model/MailApiKey.swift
+++ VerificationEmail/Model/MailApiKey.swift
@@ -8,3 +8,4 @@
 import Foundation
 
 let apiKey = "live_a0a2b2232cdcf8f170c076fe363901c6033115ffd20fcb1e96076cfd33b9d3bf"
+let apiKey2 = "test_52b675917ccd23629fad14ee86590036f142683358078b939fb51fb842369ef0"
--- VerificationEmail/Views/Alert.swift
+++ VerificationEmail/Views/Alert.swift
@@ -19,11 +19,29 @@ struct Alert {
         }
     }
     
+    static func showErrorAlert(vc: UIViewController,
+                               title: String,
+                               message: String,
+                               completion: @escaping() -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default) { _ in
+            completion()
+            
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        DispatchQueue.main.async {
+            vc.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     static func showResultAlert(vc: UIViewController, message: String) {
         showSimpleAlert(vc: vc, title: "Result", message: message)
     }
     static func showErrorAlert(vc: UIViewController, message: String, completion: @escaping() -> Void) {
-        showSimpleAlert(vc: vc, title: "Error", message: message)
+        
+        showErrorAlert(vc: vc, title: "Error", message: message, completion: completion)
     }
     
 }
